4a7f483dd75f1ed06a81c0192585a377ffb90f48
Wesley Silva <wesley.silva@ccc.ufcg.edu.br>
2017-03-07 10:33:35 -0800

    Fixed a bug with send image button from EditPointOfSaleActivity.

--- app/src/main/java/com/example/grupoes/projetoes/activities/EditPointOfSaleActivity.java
+++ app/src/main/java/com/example/grupoes/projetoes/activities/EditPointOfSaleActivity.java
@@ -1,7 +1,11 @@
 package com.example.grupoes.projetoes.activities;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,6 +22,8 @@ import com.example.grupoes.projetoes.models.PointOfSale;
 import java.util.List;
 
 public class EditPointOfSaleActivity extends AppCompatActivity {
+    private static final int RESULT_LOAD_IMAGE = 1;
+
     private EditText pSNameEditText;
     private EditText pSDescriptionEditText;
 
@@ -65,13 +71,35 @@ public class EditPointOfSaleActivity extends AppCompatActivity {
             }
         });
 
+
         pSImage.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
             }
         });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
     }
 }
